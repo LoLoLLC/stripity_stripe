fa30b34ed60ba848aa9aba7bcf705d0325f1c2eb
Josh Crews <josh@joshcrews.com>
2019-02-11 09:39:48 -0600

    remove coveralls warnings

--- mix.exs
+++ mix.exs
@@ -10,14 +10,14 @@ defmodule Stripe.Mixfile do
       elixir: "~> 1.1",
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test,
+        coveralls: :test,
+        coveralls.detail: :test,
+        coveralls.post: :test,
+        coveralls.html: :test,
         vcr: :test,
-        "vcr.delete": :test,
-        "vcr.check": :test,
-        "vcr.show": :test
+        vcr.delete: :test,
+        vcr.check: :test,
+        vcr.show: :test
       ]]
   end
 
